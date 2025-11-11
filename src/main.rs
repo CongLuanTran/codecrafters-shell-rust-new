8d31691ba1bc12194842dbbe1851fbf69b7bab11
Cong Luan Tran <congluan.tran19@gmail.com>
2025-11-11 15:15:50 +0700

    implement type

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,27 @@
 use std::collections::HashMap;
 use std::io::{self, Write};
 
-fn main() {
-    let mut builtins: HashMap<&str, fn(&str)> = HashMap::new();
-    builtins.insert("exit", exit);
-    builtins.insert("echo", echo);
+struct Builtin {
+    name: &'static str,
+    func: fn(&str),
+}
 
+static BUILTINS: [Builtin; 3] = [
+    Builtin {
+        name: "exit",
+        func: exit,
+    },
+    Builtin {
+        name: "echo",
+        func: echo,
+    },
+    Builtin {
+        name: "type",
+        func: exec_type,
+    },
+];
+
+fn main() {
     loop {
         let mut input = String::new();
         print!("$ ");
@@ -14,23 +30,28 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         for line in input.lines() {
             if let Some((cmd, args)) = line.split_once(" ") {
-                if let Some(func) = builtins.get(cmd) {
-                    func(args);
-                } else {
-                    println!("{}: command not found", cmd);
+                if let Some(builtin) = BUILTINS.iter().find(|b| b.name == cmd) {
+                    (builtin.func)(args);
+                    continue;
                 }
-            } else {
-                println!("{}: command not found", line);
             }
+            println!("{}: command not found", line);
         }
     }
 }
 
 fn exit(args: &str) {
     let code = args.parse::<i32>().unwrap_or_default();
-    std::process::exit(code)
+    std::process::exit(code);
 }
 
 fn echo(args: &str) {
     println!("{}", args)
+}
+fn exec_type(args: &str) {
+    if let Some(builtin) = BUILTINS.iter().find(|b| b.name == args) {
+        println!("{} is a shell builtin", builtin.name)
+    } else {
+        println!("{}: not found", args)
+    }
 }
