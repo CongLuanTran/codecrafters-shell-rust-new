4aaeccd92e51a3d90013f7eb971f031d3d225186
Cong Luan Tran <congluan.tran19@gmail.com>
2025-11-11 13:45:09 +0700

    implement repl

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // let builtins = [""];
-    let mut input = String::new();
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-    for line in input.lines() {
-        println!("{}: command not found", line);
+    loop {
+        let mut input = String::new();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        for line in input.lines() {
+            println!("{}: command not found", line);
+        }
     }
 }
