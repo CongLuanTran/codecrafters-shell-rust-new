ad81eab3b37e1baa9fcf42cb07d8351a3de08e06
Cong Luan Tran <congluan.tran19@gmail.com>
2025-11-11 13:43:29 +0700

    react to one command input

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@
 use std::io::{self, Write};
 
 fn main() {
+    // let builtins = [""];
+    let mut input = String::new();
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    for line in input.lines() {
+        println!("{}: command not found", line);
+    }
 }
