16cc9928bb8d9f30497abc5963b4215ec099cf91
Cong Luan Tran <congluan.tran19@gmail.com>
2025-11-11 14:07:23 +0700

    implement exit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 #[allow(unused_imports)]
+use std::collections::HashMap;
 use std::io::{self, Write};
 
 fn main() {
-    // let builtins = [""];
+    let mut builtins: HashMap<&str, fn(&str)> = HashMap::new();
+    builtins.insert("exit", exit);
+
     loop {
         let mut input = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
         for line in input.lines() {
-            println!("{}: command not found", line);
+            if let Some((cmd, args)) = line.split_once(" ") {
+                if let Some(func) = builtins.get(cmd) {
+                    func(args);
+                } else {
+                    println!("{}: command not found", cmd);
+                }
+            } else {
+                println!("{}: command not found", line);
+            }
         }
     }
+}
+
+fn exit(args: &str) {
+    let code = args.parse::<i32>().unwrap_or_default();
+    std::process::exit(code)
 }
